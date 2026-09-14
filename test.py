6c90c9d27eb9335e55b84e2b1b21e1461df5f865
Bhussai05 <b2hussain@torontomu.ca>
2026-09-14 18:15:42 -0400

    kuramoto oscillations

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import networkx as nx 
 import seaborn as sns
+import matplotlib.pyplot as plt 
+
 
 from kuramoto import Kuramoto, plot_activity
 
@@ -9,4 +11,10 @@ sns.set_context("notebook", font_scale=1.6)
 graph = nx.erdos_renyi_graph(n=100, p =0.5)
 matrix = nx.to_numpy_array(graph)
 
-model = 
+model = Kuramoto(coupling=4, dt=0.01, T=10, n_nodes=len(matrix))
+
+activity = model.run(adj_mat=matrix)
+
+plot_activity(activity)
+
+plt.show()
