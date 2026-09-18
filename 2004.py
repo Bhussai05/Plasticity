3e2b95895938a3a1b3f8640b6864fe4e8a80fcb0
Bhussai05 <b2hussain@torontomu.ca>
2026-09-18 17:37:01 -0400

    evolving weights

--- 2004.py
+++ 2004.py
@@ -1,38 +1,44 @@
 import numpy as np
 from scipy.integrate import solve_ivp
-
+import matplotlib.pyplot as plt
 
 
 rng = np.random.default_rng(7)
-N = 2
+N = 5
 K = 5
-y_0 = [5,3]
 connection = 1
 
 b = 0.1
 c = 0.2
+epsilon = 0.01
 
-M = np.zeros((N,N))
 
 for i in range(N):
     for j in range(i+1, N):
         if rng.random() < connection:
 
-            alpha = rng.random()
+            alpha_0 = rng.random()
 
             if rng.random() < 0.5:
-                predator, prey = i,j
+                predator, prey = 0,1
             else:
-                predator, prey = j,i
+                predator, prey = 1,0
 
-            M[predator, prey] = alpha
-            M[prey, predator] = -b * alpha
 
 autotroph = np.array([True,False])
 
+y_0 = [5,3, alpha_0]
 
-def rates(t,y,c,K_1):
-    x = np.array(y)
+
+def rates(t,y,c,K_1, epsilon):
+    x = y[:N]
+    alpha = y[N]
+
+    M = np.zeros((N,N))
+
+    M[predator, prey] = alpha
+    M[prey, predator] = -b * alpha
+    
 
     interactions = x * (M@x)
 
@@ -44,7 +50,9 @@ def rates(t,y,c,K_1):
         else:
             dx[i] += -c*x[i]
 
-    return dx
+        dalpha = epsilon * (x[prey] - x[predator]) * alpha
+
+    return np.concatenate((dx, [dalpha]))
 
     
 
@@ -53,8 +61,12 @@ sol = solve_ivp(
     rates,
     (0,20),
     y_0,
-    args=(c,K)
+    args=(c,K, epsilon)
 )
+plt.plot(sol.t, sol.y[0], label="x1")
+plt.plot(sol.t, sol.y[1], label="x2")
+plt.plot(sol.t, sol.y[2], label="alpha")
 
-
-
+plt.legend()
+plt.xlabel("t")
+plt.show()
